196f99d21da5eda66cae24e480df0e37c77d8162
abdullahshahid03 <abdulllahh.shahid@gmail.com>
2025-07-07 04:57:31 +0500

    initial part

---
+++ lib/quotes.ts
@@ -0,0 +1,11 @@
+export const quotes = [
+  { topic: "life", quote: "Life is what happens when you're busy making other plans." },
+  { topic: "life", quote: "To live is the rarest thing in the world. Most people exist, that is all." },
+  { topic: "life", quote: "Life itself is the most wonderful fairy tale." },
+  { topic: "success", quote: "Success is not in what you have, but who you are." },
+  { topic: "success", quote: "Don't be afraid to give up the good to go for the great." },
+  { topic: "success", quote: "Success usually comes to those who are too busy to be looking for it." },
+  { topic: "love", quote: "Love all, trust a few, do wrong to none." },
+  { topic: "love", quote: "The best thing to hold onto in life is each other." },
+  { topic: "love", quote: "We are most alive when we're in love." }
+];
